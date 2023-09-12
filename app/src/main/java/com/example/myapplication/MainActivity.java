f7e401d793df899de12e6d67560e158c8ce6172c
RafaelSoaresSMP <rafael.soares@simplesmeiosdepagamento.com.br>
2023-09-12 15:28:39 -0300

    bcgvb

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -14,6 +15,7 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.bumptech.glide.Glide;
 import com.example.myapplication.Api.MySingleton;
 import com.google.gson.Gson;
 
@@ -31,19 +33,30 @@ public class MainActivity extends AppCompatActivity {
         MySingleton.getInstance(this);
 
         TextView texto = findViewById(R.id.text);
+        ImageView image = findViewById(R.id.imageView);
 
-        String url = "https://rickandmortyapi.com/api/character/5";
 
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
-                new Response.Listener<JSONObject>() {
+        String url = "https://rickandmortyapi.com/api/character/123" ;
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
                     @Override
                     public void onResponse(JSONObject response) {
-                        texto.setText((CharSequence) response.names());
+                        try {
+                            texto.setText("Response: " + response.getString("name"));
+
+                            Glide.with(MainActivity.this).load(response.getString("image")).into(image);
+
+                        } catch (JSONException e) {
+                            throw new RuntimeException(e);
+                        }
                     }
-                },
-                new Response.ErrorListener() {
+                }, new Response.ErrorListener() {
+
                     @Override
                     public void onErrorResponse(VolleyError error) {
+                        // TODO: Handle error
 
                     }
                 });
