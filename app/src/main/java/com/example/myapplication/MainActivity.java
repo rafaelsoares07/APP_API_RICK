4f74b48f1996ee20c98285e7fa82fd6618ddd61a
RafaelSoaresSMP <rafael.soares@simplesmeiosdepagamento.com.br>
2023-09-13 15:56:43 -0300

    update loadin and buton shared person

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,71 +1,152 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
-import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 import com.bumptech.glide.Glide;
 import com.example.myapplication.Api.MySingleton;
-import com.google.gson.Gson;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     private Boolean reqStatus = false;
+   // private String urlImage;
+    private ConstraintLayout tela;
+    private View loading;
+    private TextView textName;
+    private TextView textStatus;
+    private TextView textOrigin;
+    private TextView textSpecie;
+    private Button btnFetchPerson;
+    private Button btnSharedInfo;
+    private ImageView imagePerson;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        MySingleton.getInstance(this);
 
-        TextView texto = findViewById(R.id.text);
-        ImageView image = findViewById(R.id.imageView);
+        tela = findViewById(R.id.tela);
+
+        textName = findViewById(R.id.textName);
+        textStatus = findViewById(R.id.textStatus);
+        textOrigin = findViewById(R.id.textOrigin);
+        textSpecie = findViewById(R.id.textSpecie);
+
+        btnFetchPerson = findViewById(R.id.btnFetchPerson);
+        btnSharedInfo= findViewById(R.id.btnSharedInfo);
+
+        imagePerson = findViewById(R.id.imagePerson);
+
+        loading = findViewById(R.id.loading);
+
+        btnFetchPerson.setOnClickListener(view -> {
+            loading.setVisibility(View.VISIBLE);
+            tela.setVisibility(View.GONE);
+            fetchPerson();
+        });
+
+        btnSharedInfo.setOnClickListener(view -> {
+//            Intent shareIntent = new Intent();
+//            shareIntent.setAction(Intent.ACTION_SEND);
+//            shareIntent.putExtra(Intent.EXTRA_STREAM, urlImage);
+//            shareIntent.setType("image/jpeg");
+//            startActivity(Intent.createChooser(shareIntent, "Enviar"));
+
+            Intent sendIntent = new Intent();
+            sendIntent.setAction(Intent.ACTION_SEND);
+            sendIntent.putExtra(Intent.EXTRA_TEXT,
+                    "O personagem que eu tirei foi: "+ textName.getText() + "\n" +
+                            "Esse personagem esta :" + textStatus.getText()+"\n" +
+                            "E a sua origem é esta: " +textOrigin.getText());
+            sendIntent.setType("text/plain");
+
+            Intent shareIntent = Intent.createChooser(sendIntent, null);
+            startActivity(shareIntent);
+
+        });
 
 
-        String url = "https://rickandmortyapi.com/api/character/123" ;
+    }
+
+    private void fetchPerson(){
+        int randomNumber = new Random().nextInt(500);
+        String url = "https://rickandmortyapi.com/api/character/" + randomNumber ;
+
+        Log.i("urlç", url);
 
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
-                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                (Request.Method.GET, url, null, response -> {
+                    try {
+                        Log.i("api", response.toString());
+                        textName.setText(response.getString("name"));
+                        textStatus.setText(response.getString("status"));
+                        textSpecie.setText(response.getString("species"));
 
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        try {
-                            texto.setText("Response: " + response.getString("name"));
+                        loading.setVisibility(View.GONE);
+                        tela.setVisibility(View.VISIBLE);
+                        btnSharedInfo.setEnabled(true);
 
-                            Glide.with(MainActivity.this).load(response.getString("image")).into(image);
+                        JSONObject jsonObjectOrigin = response.getJSONObject("origin");
+                        String origin = jsonObjectOrigin.getString("name");
+                        textOrigin.setText(origin);
 
-                        } catch (JSONException e) {
-                            throw new RuntimeException(e);
-                        }
+                        //urlImage = response.getString("image");
+
+                        Glide.with(MainActivity.this).load(response.getString("image")).into(imagePerson);
+
+                    } catch (JSONException e) {
+                        loading.setVisibility(View.GONE);
+                        btnSharedInfo.setEnabled(false);
+                        throw new RuntimeException(e);
                     }
-                }, new Response.ErrorListener() {
+                }, error -> {
+                    // TODO: Handle error
+                    loading.setVisibility(View.GONE);
+                    btnSharedInfo.setEnabled(false);
 
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        // TODO: Handle error
-
-                    }
                 });
 
-
         MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
-
-
     }}
+
+//    public  boolean isInternetConnection() {
+//
+//        Context context = null;
+//
+//        if (context == null) {
+//            return false;
+//        }
+//        else {
+//            ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+//            if (!connectivityManager.getActiveNetworkInfo().isConnectedOrConnecting()) {
+//                return false;
+//            } else {
+//                return true;
+//            }
+//
+//        }
+//    }
+//}
 
 
 
